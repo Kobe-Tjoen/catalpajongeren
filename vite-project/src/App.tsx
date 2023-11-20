02d2b0c91f0b0ec05b67b28b338d8034bf557495
Kobe-Tjoen <kobe.tjoen@student.ucll.be>
2023-11-20 14:02:26 +0100

    might be the solution

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -10,7 +10,7 @@ function App() {
     <>
       <Header/>
       <main className='h-full pt-28 md:pt-40'>
-        <div className='argent custom-bg h-screen w-full bg-cover bg-fixed'>
+        <div className='argent custom-bg h-screen bg-cover bg-fixed'>
           <h1 className='text-3xl md:text-4xl font-bold pt-40 text-center text-white'>Groene haven voor jongeren.</h1>
         </div>
         <Missie/>
