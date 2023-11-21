cf71f97f974f49c1ca274cad3ca9fa41f0620cfa
Kobe-Tjoen <kobe.tjoen@student.ucll.be>
2023-11-21 10:59:04 +0100

    large screen paralax scrolling

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -10,7 +10,7 @@ function App() {
     <>
       <Header/>
       <main className='h-full pt-28 md:pt-40'>
-        <div className='argent custom-bg h-screen bg-cover'>
+        <div className='argent custom-bg h-screen bg-cover md:bg-fixed'>
           <h1 className='text-3xl md:text-4xl font-bold pt-40 text-center text-white'>Groene haven voor jongeren.</h1>
         </div>
         <Missie/>
